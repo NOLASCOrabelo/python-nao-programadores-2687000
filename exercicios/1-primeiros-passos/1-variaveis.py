212e01359e4f7c16782b42cbb8107f58c31895eb
Nolasco <133284431+NOLASCOrabelo@users.noreply.github.com>
2025-07-29 03:04:37 +0000

    Resolução da Variavel

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -1,7 +1,3 @@
-# Convenções para nomear as variáveis:
-# - Iniciar sempre com letra minúscula;
-# - O nome pode ser composto por letras, algarismos e underlines;
-# - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 nome = 'Thiago' 
 obejetivo = "Mudar de profissão"
