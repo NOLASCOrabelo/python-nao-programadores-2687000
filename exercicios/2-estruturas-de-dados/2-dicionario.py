5870e52b6414c1c1363dadf3a36922f45ebf0acc
Nolasco <133284431+NOLASCOrabelo@users.noreply.github.com>
2025-08-06 22:39:47 +0000

    Dicionary about me

--- exercicios/2-estruturas-de-dados/2-dicionario.py
+++ exercicios/2-estruturas-de-dados/2-dicionario.py
@@ -1,18 +1,22 @@
-pessoa = {'nome':'Crislaine', 
-          'idade':46, 
-          'ano_formatura':2010, 
-          'linguagens_programacao':['python', 'r', 'javascript', 'ruby'], 
-          'brasileira':True, 
-          'hobby':['nadar', 'ler', 'pedalar'], 
-          'animal_estimacao':False}
+pessoa = {'name':'Mercuri', 
+          'old':20, 
+          'year_formation':2026, 
+          'Tools':['python', 'PowerBI', 'Excel', 'ETL'], 
+          'Brazilian':True, 
+          'musics':['Where have you been', 'dont stop music', 'mesma fita'], 
+          'God not exist':False}
 
 # Imprima na tela o valor equivalente a chave "hobby"
-
+print(pessoa['musics'])
 
 # Imprima na tela uma lista apenas com os valores do dicionário
+print(pessoa.values())
 
 
 # Imprima na tela uma lista apenas com as chaves do dicionário
+print(pessoa.keys())
 
 
 # Insira um novo par chave-valor no dicionário
+pessoa['ETL'] = 'PowerBI'
+print(pessoa)
