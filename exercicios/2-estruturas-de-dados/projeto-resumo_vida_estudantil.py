103249b89a1fba4ff223cc1a23341935d74de242
Nolasco <133284431+NOLASCOrabelo@users.noreply.github.com>
2025-08-07 01:29:50 +0000

    Atualização

--- exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
+++ exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
@@ -1,20 +1,17 @@
 
-estudante = { }
+cliente = { }
 
-
-estudante['nome'] = input('Qual seu nome?')
-estudante['ano_que_conheceu_o_linkedin'] = int(input('Quando conheceu o Linkedin?' ))
-estudante['ano_atual'] = int(input('E o ano atual? '))
-cursos = input('Quais cursos realizou?Todos separados por virgula e ordem cronologica:')
- 
-estudante['cursos'] = cursos.split(', ')
+cliente['nome'] = input('Seu nome: ')
+cliente['valor_de_renda'] = int(input('Valor a ser aplicado: '))
+cliente['investimento'] = int(input('Tempo que deseja investir(em meses): '))
 
 
 
 
 
 
-total_anos = estudante['ano_atual'] - estudante['ano_que_conheceu_o_linkedin']
-total_cursos = len(estudante['cursos'])
 
-print(f"Como vai {estudante['nome']}, o ano que conheceu o Linkedin foi {estudante['ano_que_conheceu_o_linkedin']}, Durante esses {total_anos} anos, você realizou {total_cursos} cursos, sendo o primeiro curso realizado {estudante['cursos'][0]}, e o último sendo {estudante['cursos'][-1]}")
+total_valor = cliente['valor_de_renda'] * cliente['investimento']
+
+
+print(f"Segue os números {cliente['nome']}, com você aplicando {cliente['valor_de_renda']}, por {cliente['investimento']} mes(s), você terá {total_valor}")
