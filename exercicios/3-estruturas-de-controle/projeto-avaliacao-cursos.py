e27e1721e45f35d4889b647592e0d89798b16601
Nolasco <133284431+NOLASCOrabelo@users.noreply.github.com>
2025-08-08 03:45:24 +0000

    Explaining the code

--- exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
+++ exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
@@ -1,11 +1,16 @@
+
+# criação de lista
 Cliente = ['Thiago', '20 anos', 'cabelo preto', 'nariz grande', 'cílios']
 
+#criação de variaveis para serem usadas junto com a lista
 rosto = 'nariz grande'
 nome = 'Thiago'
 idade = '20 anos'
 
+# Dicionario vazio, para ser inplementado durante o code
 Pessoa = {}
 
+# estrutura condicional para verificar se tal item está na lista e se estiver, deverá avaliar com uma nota
 if rosto in Cliente:
   print(f'A característica {rosto} existe. Responda abaixo o que acha dela ') 
   Pessoa [rosto] = int(input('Qual a nota para essa carecterística? '))
@@ -17,6 +22,12 @@ if idade in Cliente:
 if nome in Cliente:
   print(f'A característica {nome} existe. Responda abaixo o que acha dela ') 
   Pessoa [nome] = int(input('Qual a nota para essa carecterística? '))
+
+# Caso não estiver, mensagem de "Erro"
 else:
   print(f'Essa carecterística não existe X')
+
+# Imprimir na tela o dicionário criado durante o code
+print(Pessoa)
+
  
