1e983234a4b9c632cc159d0da5699df1885a80a1
Nolasco <133284431+NOLASCOrabelo@users.noreply.github.com>
2025-08-08 03:40:12 +0000

    Resolution exercices of project

--- exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
+++ exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
@@ -1,7 +1,22 @@
-# Nesse desafio você verificará dentro de uma lista se o item estar contido nela, caso verdadeiro deverá imprimir na tela essa informação, além disso deverá solicitar avaliação para o item e armazená-la em um dicionário.
-# 1. Crie uma lista com 5 diferentes cursos do LinkedIn Learning
-# 2. Crie 3 variáveis do tipo string e associe 1 curso a cada uma delas
-# 3. Crie um dicionário vazio para armazenar a nota do curso
-# 4. Crie uma estrutura condicional para verificar se cada variável está contida na lista
-# 5. Se o curso estiver na lista, solicite uma nota para avaliação
-# 6. Armazene essa nota no dicionário, sendo a chave o título do curso e o valor a nota
+Cliente = ['Thiago', '20 anos', 'cabelo preto', 'nariz grande', 'cílios']
+
+rosto = 'nariz grande'
+nome = 'Thiago'
+idade = '20 anos'
+
+Pessoa = {}
+
+if rosto in Cliente:
+  print(f'A característica {rosto} existe. Responda abaixo o que acha dela ') 
+  Pessoa [rosto] = int(input('Qual a nota para essa carecterística? '))
+
+if idade in Cliente:
+  print(f'A característica {idade} existe. Responda abaixo o que acha dela ') 
+  Pessoa [idade] = int(input('Qual a nota para essa carecterística? '))
+
+if nome in Cliente:
+  print(f'A característica {nome} existe. Responda abaixo o que acha dela ') 
+  Pessoa [nome] = int(input('Qual a nota para essa carecterística? '))
+else:
+  print(f'Essa carecterística não existe X')
+ 
