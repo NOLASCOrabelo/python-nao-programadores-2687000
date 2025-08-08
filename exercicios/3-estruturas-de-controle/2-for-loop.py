d19a5260627b2c61fcbaa03ddb421a2ad9c0ca5e
Nolasco <133284431+NOLASCOrabelo@users.noreply.github.com>
2025-08-08 02:37:52 +0000

    Resolução exercício "For"

--- exercicios/3-estruturas-de-controle/2-for-loop.py
+++ exercicios/3-estruturas-de-controle/2-for-loop.py
@@ -1,9 +1,10 @@
-# Crie uma lista
-
-# Crie um for loop para imprimir cada elemento dessa lista
+numeros = [1, 2, 3, 4, 5, 6, 7]
 
 
-# Crie um objeto iterável utilizando a função range()
+for numero in numeros:
+  print(numeros)
 
 
-# Use esse objeto iterável para criar um for loop que imprima na tela a frase "Estou aprendendo uma linguagem de programação."
+for numeros in range(7):
+  print(f'- {numeros}. Estou aprendendo a contar')
+
